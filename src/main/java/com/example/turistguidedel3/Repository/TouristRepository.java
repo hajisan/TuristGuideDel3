8a8b1ff705af7ce0a5c24cac08d511c149c7ca79
Nima <nimaginobilisalami@gmail.com>
2025-03-20 17:38:59 +0100

    Rettet application.properties
    For at ignorere min database, så hjemmesiden kan loades efter deployment

--- src/main/java/com/example/turistguidedel3/Repository/TouristRepository.java
+++ src/main/java/com/example/turistguidedel3/Repository/TouristRepository.java
@@ -3,6 +3,7 @@ package com.example.turistguidedel3.Repository;
 import com.example.turistguidedel3.Model.Attraction;
 import com.example.turistguidedel3.Model.City;
 import com.example.turistguidedel3.Model.Tag;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
+@ConditionalOnProperty(name = "spring.datasource.url", havingValue = "none", matchIfMissing = true)
 public class TouristRepository {
     private final JdbcTemplate jdbcTemplate;
 
